1d26b2c09b89b517c20e8a47275ae4236ee07dc8
aback1227 <aback1227@gmail.com>
2016-12-06 21:44:00 -0500

    capitalize 'r' in ActiveRecord

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ ENV['SINATRA_ENV'] ||="development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-Activerecord::Base.establish_connection(
+ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )

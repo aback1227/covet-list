06843b7cce476717d88a56eaf06336e92dfcac2b
aback1227 <aback1227@gmail.com>
2016-12-09 00:50:11 -0500

    enable sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < Sinatra::Base
   configure do
       set :views, "app/views"
       set :public_dir, "public"
+      enable :sessions
+      set :session_secret, "cookie"
   end
 
   get '/' do

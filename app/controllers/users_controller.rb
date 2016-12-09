ca02821ed062fe6208ae41d6b90337b15ab0e8f6
aback1227 <aback1227@gmail.com>
2016-12-09 00:46:44 -0500

    create post /signup route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,17 @@ class UsersController < ApplicationController
     erb :'/user/create_user'
   end
 
+  post '/signup' do
+    @user = User.new(username: params[:username], email: params[:email], firstname: params[:firstname], lastname: params[:lastname], password: params[:password], birthday: params[:birthday])
+    if @user.save
+      redirect '/show'
+    else 
+      redirect '/signup'
+    end
+  end
+
+  get '/show' do
+    erb :'/user/show_user'
+  end
+
 end

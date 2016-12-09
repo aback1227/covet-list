738623d52af8eb455f0efe16a4065b4300b21184
aback1227 <aback1227@gmail.com>
2016-12-09 00:56:22 -0500

    modify routes to check for logged in user, add logout route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
 class UsersController < ApplicationController
 
   get '/signup' do
-    erb :'/user/create_user'
+    if session[:user_id]
+      redirect '/show'
+    else
+      erb :'/user/create_user'
+    end
   end
 
   post '/signup' do
     @user = User.new(username: params[:username], email: params[:email], firstname: params[:firstname], lastname: params[:lastname], password: params[:password], birthday: params[:birthday])
     if @user.save
+      session[:user_id] = @user.id
       redirect '/show'
     else 
       redirect '/signup'
@@ -15,6 +20,15 @@ class UsersController < ApplicationController
 
   get '/show' do
     erb :'/user/show_user'
+  end
+
+  get 'logout' do
+    if session[:user_id]
+      session.clear
+      redirect '/login'
+    else 
+      redirect '/'
+    end
   end
 
 end

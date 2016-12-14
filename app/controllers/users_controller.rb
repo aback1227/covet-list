ae666e38208e991cb6d9633939295d3b8465861e
aback1227 <aback1227@gmail.com>
2016-12-13 22:38:33 -0500

    refactor with helpers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,53 +1,46 @@
 class UsersController < ApplicationController
 
   get "/signup" do
-    if session[:user_id]
-      @user = User.find(session[:user_id])
-      redirect "/#{@user.slug}"
-      # redirect "/show"
+    if logged_in?
+      redirect "/user/#{current_user.slug}"
     else
-      erb :"/user/create_user"
+      erb :'/user/create_user'
     end
   end
 
   post "/signup" do
-    @user = User.new(username: params[:username], email: params[:email], firstname: params[:firstname], lastname: params[:lastname], password: params[:password], birthday: params[:birthday])
-    if @user.save
-      session[:user_id] = @user.id
-      @user = User.find(session[:user_id])
-      redirect "/#{@user.slug}"
-      # redirect "/show"
+    user = User.new(username: params[:username], email: params[:email], firstname: params[:firstname], lastname: params[:lastname], password: params[:password], birthday: params[:birthday])
+    if user.save
+      session[:user_id] = user.id
+      redirect "/user/#{current_user.slug}"
     else 
       redirect "/signup"
     end
   end
 
   get "/login" do
-    if session[:user_id]
-      @user = User.find(session[:user_id])
-      redirect "/#{@user.slug}"
+    if logged_in?
+      redirect "/user/#{current_user.slug}"
     else 
-      erb :"/user/login"
+      erb :'/user/login'
     end
   end
 
   post "/login" do
     user = User.find_by(username: params[:username])
-    if user
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      # redirect "/#{user.slug}"
-      redirect "/show"
+      redirect "/user/#{current_user.slug}"
     else
       redirect "/login"
     end
   end
 
-  get "/:slug" do
-    @user = User.find_by_slug(params[:slug])
-    erb :"/user/show_user"
+  get '/user/:slug' do
+    erb :'/user/show_user'
   end
 
-  get "/logout" do
+  get '/logout' do
     session.clear
     redirect "/"
   end

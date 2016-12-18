4c0700667e5a964a39862c558ec0d452aab04b24
aback1227 <aback1227@gmail.com>
2016-12-18 00:58:40 -0500

    remove get /wishlists and redirect to user's show page

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -3,16 +3,12 @@ class WishlistsController < ApplicationController
     erb :'/wishlist/create_wishlist'
   end
 
-  get '/wishlists' do
-    erb :'/wishlist/wishlists'
-  end
-
   post '/wishlists' do
     if logged_in?
       wishlist = Wishlist.create(title: params[:title], description: params[:description], user_id: session[:user_id])
       wishlist.user = current_user
       if wishlist.save
-        redirect '/wishlists'
+        redirect "/users/#{current_user.slug}"
       else
         redirect '/wishlists/new'
       end

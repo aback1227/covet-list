5661f4c7c67e83350e0d84ce3b03ead9ae13168a
aback1227 <aback1227@gmail.com>
2016-12-18 00:52:15 -0500

    get and post /wishlists routes, views, and css

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -4,6 +4,21 @@ class WishlistsController < ApplicationController
   end
 
   get '/wishlists' do
+    erb :'/wishlist/wishlists'
+  end
+
+  post '/wishlists' do
+    if logged_in?
+      wishlist = Wishlist.create(title: params[:title], description: params[:description], user_id: session[:user_id])
+      wishlist.user = current_user
+      if wishlist.save
+        redirect '/wishlists'
+      else
+        redirect '/wishlists/new'
+      end
+    else
+      redirect '/login'
+    end
   end
 
   get '/wishlists/:id' do

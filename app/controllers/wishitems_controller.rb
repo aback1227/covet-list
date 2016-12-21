11eb7027147a1a0a0e858742b56d7c2d15e69daa
aback1227 <aback1227@gmail.com>
2016-12-21 00:32:51 -0500

    add wishitem edit page, route, view form

--- app/controllers/wishitems_controller.rb
+++ app/controllers/wishitems_controller.rb
@@ -7,4 +7,35 @@ class WishItemsController < ApplicationController
   get '/wishitems/:id' do
     erb :'/wishitem/show_wishitem'
   end
+
+  get '/wishitems/:id/edit' do
+    if logged_in?
+      @wishitem = WishItem.find_by_id(params[:id])
+      erb :'/wishitem/edit_wishitem'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/wishitems/:id' do
+    if logged_in?
+      if wishitem_updated
+        redirect "/wishlists/#{current_wishitem.wishlist.id}"
+      else 
+        redirect "/wishitems/#{current_wishitem.id}/edit"
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  helpers do 
+    def current_wishitem
+      WishItem.find_by_id(params[:id])
+    end
+
+    def wishitem_updated
+      current_wishitem.update(title: params[:title], url: params[:url], price: params[:price], quantity: params[:quantity], image: params[:image], note: params[:note])
+    end
+  end
 end

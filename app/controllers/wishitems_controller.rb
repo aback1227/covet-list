5d494a1908c2e401420c8e3cca8e0a08f877d3eb
aback1227 <aback1227@gmail.com>
2016-12-24 01:11:09 -0500

    remove get wishitems/new route

--- app/controllers/wishitems_controller.rb
+++ app/controllers/wishitems_controller.rb
@@ -1,9 +1,5 @@
 class WishItemsController < ApplicationController
 
-  # get '/wishitems/new' do
-  #   erb :'/wishitem/create_wishitem'
-  # end
-
   # get '/wishitems/:id' do
   #   erb :'/wishitem/show_wishitem'
   # end

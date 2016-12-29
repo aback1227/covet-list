9b1c98e82e9f8401a6f32f552a05adfad17dd4a8
aback1227 <aback1227@gmail.com>
2016-12-28 21:08:15 -0500

    find_by_slug

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   end
 
   def self.find_by_slug(slug)
-    self.all.find {|instance| instance.slug == slug}
+    self.all.find {|user| user.slug == slug}
   end
 end
+

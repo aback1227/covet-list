96d66392f16354d979c30509f3438d61e485f56c
aback1227 <aback1227@gmail.com>
2017-01-10 21:18:50 -0500

    replace @wishitem with helper method

--- app/controllers/wishitems_controller.rb
+++ app/controllers/wishitems_controller.rb
@@ -21,7 +21,6 @@ class WishItemsController < ApplicationController
   get '/wishitems/:id/edit' do
     if logged_in?
       if current_user == current_wishitem.wishlist.user
-        @wishitem = WishItem.find_by_id(params[:id])
         erb :'/wishitem/edit_wishitem'
       else
         redirect "wishlists/#{current_wishlist.id}/wishitems"
